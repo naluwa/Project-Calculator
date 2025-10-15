8933cefb29fb139266f4f05047ce3c3f8d1127d8
naluwa <tharushadilshan4u@gmail.com>
2025-10-15 17:40:24 +0530

    adding cal menu and the switch case

--- main.c
+++ main.c
@@ -1,8 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
 int main()
 {
-    printf("Hello world!\n");
+    int choice;
+    printf("------Calculator Menu------\n\n");
+    printf("1.Addition\n");
+    printf("2.Subtraction\n");
+    printf("3.Multiplication\n");
+    printf("4.Division\n");
+    printf("5.Exponentiation\n");
+    printf("6.Factorial\n");
+    printf("7.Exit\n\n");
+    printf("---------------------------\n\n");
+    printf("Enter your choice :");
+    scanf("%d", &choice);
+
+    do {
+            switch(choice){
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            break;
+        case 7:
+            break;
+        default:
+            printf("Invalid choice.. Try again");
+
+            }
+
+    }while(choice != 7);
     return 0;
 }
